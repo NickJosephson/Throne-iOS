91a4188a15bd8496979f52dbab622b33cef07b12
Nicholas Josephson <josephs6@myumanitoba.ca>
2020-01-30 16:07:41 -0600

    Initial Commit

---
+++ Throne/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Throne
+//
+//  Created by Nicholas Josephson on 2020-01-30.
+//  Copyright © 2020 Throne. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
