d8ed46227e771906a718c1e90d8aaf536566ff39
Nicholas Josephson <josephs6@myumanitoba.ca>
2020-01-30 18:34:23 -0600

    Add util to fetch data

---
+++ Throne/Utils.swift
@@ -0,0 +1,34 @@
+//
+//  Utils.swift
+//  Throne
+//
+//  Created by Nicholas Josephson on 2020-01-30.
+//  Copyright © 2020 Throne. All rights reserved.
+//
+
+import Foundation
+
+/// Fetch Data using a given URL
+/// - Parameters:
+///   - url: URL it post GET request to
+///   - completionHandler: Function to handle Data once received.
+func fetch(url: URL, completionHandler: @escaping (Data) -> Void) {
+    let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+        if let error = error {
+            print("Error with fetching: \(error)")
+            return
+        }
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+            (200...299).contains(httpResponse.statusCode) else {
+                print("Error with the response, unexpected status code: \(String(describing: response))")
+                return
+        }
+        
+        if let data = data {
+            completionHandler(data)
+        }
+    })
+    
+    task.resume()
+}
