b60ac3aa48c65a12c610e26b3b23054e2060d299
Nicholas Josephson <josephs6@myumanitoba.ca>
2020-02-17 13:01:18 -0600

    Add more debugging output

--- Throne/Models/NearMe.swift
+++ Throne/Models/NearMe.swift
@@ -18,8 +18,10 @@ final class NearMe: ObservableObject {
     @Published var washrooms: [Washroom] = []
     
     private func fetchWashrooms() {
+        print("Fetching washrooms.")
         fetchStrings(at: url) { allWashrooms in
             DispatchQueue.main.async {
+                print("Setting washrooms.")
                 self.washrooms = allWashrooms.map {
                     return Washroom(id: 0, title: $0, location: Location(latitude: 0, longitude: 0), gender: .all, floor: 0, buildingID: 0, createdAt: Date(), overallRating: 0, averageRatings: [:], amenities: [])
                 }
