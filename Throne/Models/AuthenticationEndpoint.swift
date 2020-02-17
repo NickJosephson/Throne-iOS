6d42002a39fa745f7cc7e425ea8e2e30f56f8783
Nicholas Josephson <nickgjosephson@gmail.com>
2020-02-17 03:54:52 -0600

    Document AuthenticationEndpoint

--- Throne/Models/AuthenticationEndpoint.swift
+++ Throne/Models/AuthenticationEndpoint.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+/// Provides interaction with the AWS Cognito OAuth2 authentication interface for Throne
 class AuthenticationEndpoint {
     private static let settings = PersistentSettings()
     private static let host = URL(string: "https://login.findmythrone.com")!
@@ -15,6 +16,7 @@ class AuthenticationEndpoint {
     private static let scope = "email+openid"
     private static let redirect = "throne://"
     
+    /// URL of the AWS Cognito hosted login webpage
     class var loginAddress: URL {
         get {
             var urlComponents = URLComponents(url: host, resolvingAgainstBaseURL: true)!
@@ -29,6 +31,7 @@ class AuthenticationEndpoint {
         }
     }
     
+    /// URL of the AWS Cognito hosted signup webpage
     class var signupAddress: URL {
         get {
             var urlComponents = URLComponents(url: host, resolvingAgainstBaseURL: true)!
@@ -43,6 +46,13 @@ class AuthenticationEndpoint {
         }
     }
     
+    /// Asynchronously fetch id/access/refresh tokens.
+    ///
+    /// * The access token is needed to authenticate calls to the Throne API.
+    /// * The refresh token is needed for fetching new access tokens (which expire frequently).
+    /// - Parameters:
+    ///   - code: The AWS authorization code returned from a user login.
+    ///   - completionHandler: Handle the TokensResponse when fetching is finished.
     class func fetchTokens(authorizedWith code: String, completionHandler: @escaping (TokensResponse) -> Void) {
         var urlComponents = URLComponents(url: host, resolvingAgainstBaseURL: true)!
         urlComponents.path = "/oauth2/token"
@@ -60,10 +70,16 @@ class AuthenticationEndpoint {
         performRequest(with: request) { data in
             if let tokensResponse = try? JSONDecoder().decode(TokensResponse.self, from: data) {
                 completionHandler(tokensResponse)
+            } else {
+                print("Error decoding fetch tokens response.")
             }
         }
     }
     
+    /// Asynchronously fetch a new access token.
+    /// - Parameters:
+    ///   - refreshToken: AWS Cognito refresh token from original fetchTokens() call.
+    ///   - completionHandler: Handle the TokensResponse when fetching is finished.
     class func refreshTokens(authorizedWith refreshToken: String, completionHandler: @escaping (TokensResponse) -> Void) {
         var urlComponents = URLComponents(url: host, resolvingAgainstBaseURL: true)!
         urlComponents.path = "/oauth2/token"
@@ -80,10 +96,13 @@ class AuthenticationEndpoint {
         performRequest(with: request) { data in
             if let tokensResponse = try? JSONDecoder().decode(TokensResponse.self, from: data) {
                 completionHandler(tokensResponse)
+            } else {
+                print("Error decoding refresh tokens response.")
             }
         }
     }
     
+    /// Represents tokens returned by AWS Cognito.
     struct TokensResponse: Codable {
         let idToken: String
         let accessToken: String
