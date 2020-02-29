57a08ac747b7d434bdf15ba5cb65faf21ba63c17
Tyler Loewen <tylerscottloewen@gmail.com>
2020-02-28 21:40:54 -0600

    Complete grandient background toggling

--- Throne/Views/Onboarding/WelcomeView.swift
+++ Throne/Views/Onboarding/WelcomeView.swift
@@ -9,50 +9,45 @@
 import SwiftUI
 
 extension Color {
-    static let yellow1 = Color.yellow
-    static let darkGrey1 = Color.init(UIColor.darkGray)
-
-    static func backgroundColor(for colorScheme: ColorScheme) -> Color {
+    static func backgroundGradientColor(for colorScheme: ColorScheme) -> LinearGradient {
         if colorScheme == .dark {
-            return darkGrey1
+            return LinearGradient(gradient: Gradient(colors: [
+                        Color(red: 0.0, green: 0.1529411765, blue: 0.3019607843, opacity: 1.0),
+                        Color(red: 0, green: 0.2941176471, blue: 0.4235294118, opacity: 1.0)]
+                    ), startPoint: .bottom, endPoint: .top)
         } else {
-            return yellow1
-        }
-    }
-    
-    static func foregroundColor(for colorScheme: ColorScheme) -> Color {
-        if colorScheme == .dark {
-            return darkGrey1
-        } else {
-            return yellow1
+            return LinearGradient(gradient: Gradient(colors: [
+                        Color(red: 0.3647058824, green: 0.6392156863, blue: 0.9058823529, opacity: 1.0),
+                        Color(red: 0.4862745098, green: 0.8392156863, blue: 1, opacity: 1.0)]
+                    ), startPoint: .top, endPoint: .bottom)
         }
     }
 }
 
 struct WelcomeView: View {
-    var loginController = LoginViewController()
-    
     @Environment (\.colorScheme) var colorScheme:ColorScheme
+    
+    var loginController = LoginViewController()
     
     var body: some View {
         HStack {
             Spacer()
             VStack() {
                 Spacer()
-                    .frame(height: 100)
+                    .frame(height: 175)
                 Text("Welcome to Throne")
                     .font(.system(size: 60))
                     .foregroundColor(Color.white)
                     .multilineTextAlignment(.center)
                 Spacer()
-                    .frame(height: 100)
+                    .frame(height: 150)
                 Button(action: { self.loginController.startLogin() }) {
                     Text("Login")
                         .font(.system(size: 25))
                         .frame(width: 200.0, height: 20.0)
                 }
                     .padding()
-                    .foregroundColor(Color.foregroundColor(for: colorScheme))
+                    .foregroundColor(Color.white)
                     .background(Color(red: 0.1019607843, green: 0.5647058824, blue: 1, opacity: 1.0))
                     .cornerRadius(30)
                     .padding(5)
@@ -62,7 +57,7 @@ struct WelcomeView: View {
                         .frame(width: 200.0, height: 20.0)
                 }
                     .padding()
-                    .foregroundColor(Color.foregroundColor(for: colorScheme))
+                    .foregroundColor(Color.black)
                     .background(Color.white)
                     .cornerRadius(30)
                     .padding(5)
@@ -71,11 +66,7 @@ struct WelcomeView: View {
             }
             Spacer()
         }
-        .background((Color.backgroundColor(for: colorScheme))).edgesIgnoringSafeArea(.all)
-//        .background(LinearGradient(gradient: Gradient(colors: [
-//            Color(red: 0.3647058824, green: 0.6392156863, blue: 0.9058823529, opacity: 1.0),
-//            Color(red: 0.4862745098, green: 0.8392156863, blue: 1, opacity: 1.0)]
-//        ), startPoint: .bottom, endPoint: .top).edgesIgnoringSafeArea(.all))
+        .background((Color.backgroundGradientColor(for: colorScheme))).edgesIgnoringSafeArea(.all)
     }
 }
 
