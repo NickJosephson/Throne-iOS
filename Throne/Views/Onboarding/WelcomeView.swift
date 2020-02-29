16cdcff09f701baafbbda9022d45d94aeff8dcca
Nicholas Josephson <nickgjosephson@gmail.com>
2020-02-28 22:26:20 -0600

    Make UI more dynamic for landscape view, iPads, and large text sizes

--- Throne/Views/Onboarding/WelcomeView.swift
+++ Throne/Views/Onboarding/WelcomeView.swift
@@ -8,22 +8,6 @@
 
 import SwiftUI
 
-extension Color {
-    static func backgroundGradientColor(for colorScheme: ColorScheme) -> LinearGradient {
-        if colorScheme == .dark {
-            return LinearGradient(gradient: Gradient(colors: [
-                        Color(red: 0.0, green: 0.1529411765, blue: 0.3019607843, opacity: 1.0),
-                        Color(red: 0, green: 0.2941176471, blue: 0.4235294118, opacity: 1.0)]
-                    ), startPoint: .bottom, endPoint: .top)
-        } else {
-            return LinearGradient(gradient: Gradient(colors: [
-                        Color(red: 0.3647058824, green: 0.6392156863, blue: 0.9058823529, opacity: 1.0),
-                        Color(red: 0.4862745098, green: 0.8392156863, blue: 1, opacity: 1.0)]
-                    ), startPoint: .top, endPoint: .bottom)
-        }
-    }
-}
-
 struct WelcomeView: View {
     @Environment (\.colorScheme) var colorScheme:ColorScheme
     
@@ -34,32 +18,30 @@ struct WelcomeView: View {
             Spacer()
             VStack() {
                 Spacer()
-                    .frame(height: 175)
                 Text("Welcome to Throne")
                     .font(.system(size: 60))
-                    .foregroundColor(Color.white)
                     .multilineTextAlignment(.center)
+                    .foregroundColor(Color.white)
                 Spacer()
-                    .frame(height: 150)
                 Button(action: { self.loginController.startLogin() }) {
                     Text("Login")
-                        .font(.system(size: 25))
-                        .frame(width: 200.0, height: 20.0)
+                        .font(.title)
+                        .frame(minWidth: 200.0)
                 }
                     .padding()
                     .foregroundColor(Color.white)
                     .background(Color(red: 0.1019607843, green: 0.5647058824, blue: 1, opacity: 1.0))
-                    .cornerRadius(30)
+                    .cornerRadius(40)
                     .padding(5)
                 Button(action: { self.loginController.startSignup() }) {
                     Text("Sign Up")
-                        .font(.system(size: 25))
-                        .frame(width: 200.0, height: 20.0)
+                        .font(.title)
+                        .frame(minWidth: 200.0)
                 }
                     .padding()
                     .foregroundColor(Color.black)
                     .background(Color.white)
-                    .cornerRadius(30)
+                    .cornerRadius(40)
                     .padding(5)
                 LoginView(controller: loginController).frame(width: 0, height: 0, alignment: .bottom)
                 Spacer()
@@ -73,5 +55,21 @@ struct WelcomeView: View {
 struct WelcomeView_Previews: PreviewProvider {
     static var previews: some View {
         WelcomeView()
+    }
+}
+
+extension Color {
+    static func backgroundGradientColor(for colorScheme: ColorScheme) -> LinearGradient {
+        if colorScheme == .dark {
+            return LinearGradient(gradient: Gradient(colors: [
+                        Color(red: 0.0, green: 0.1529411765, blue: 0.3019607843, opacity: 1.0),
+                        Color(red: 0, green: 0.2941176471, blue: 0.4235294118, opacity: 1.0)]
+                    ), startPoint: .bottom, endPoint: .top)
+        } else {
+            return LinearGradient(gradient: Gradient(colors: [
+                        Color(red: 0.3647058824, green: 0.6392156863, blue: 0.9058823529, opacity: 1.0),
+                        Color(red: 0.4862745098, green: 0.8392156863, blue: 1, opacity: 1.0)]
+                    ), startPoint: .top, endPoint: .bottom)
+        }
     }
 }
