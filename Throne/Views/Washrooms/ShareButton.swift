529b13865c26e81c64c585baf8b2d484e536cd30
Nicholas Josephson <nickgjosephson@gmail.com>
2020-03-24 16:22:22 -0500

    Add macOS support

---
+++ Throne/Views/Washrooms/ShareButton.swift
@@ -0,0 +1,41 @@
+//
+//  ShareButton.swift
+//  Throne
+//
+//  Created by Nicholas Josephson on 2020-03-24.
+//  Copyright © 2020 Throne. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShareButton: View {
+    @ObservedObject var washroom: Washroom
+    @State private var showShareSheet = false
+
+    var body: some View {
+        Button(
+            action: {
+                self.showShareSheet = true
+            },
+            label: {
+                Image(systemName: "square.and.arrow.up")
+                    .accessibility(label: Text("Share Washroom"))
+            }
+        )
+        .popover(isPresented: $showShareSheet) {
+            ShareView(
+                activityItems: [self.washroom.webURL],
+                callback: {
+                    self.showShareSheet = false
+                }
+            )
+            .frame(minWidth: 350, minHeight: 600)
+        }
+    }
+}
+
+struct ShareButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ShareButton(washroom: Washroom())
+    }
+}
